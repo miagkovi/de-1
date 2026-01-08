481a1c1c764bfd0d1775bf9be590548591ee19df
Eugene Softley <ievgenii.miagkov@gmail.com>
2026-01-08 13:01:53 +0000

    ignore data

--- src/db/engine.py
+++ src/db/engine.py
@@ -1,3 +1,8 @@
 """
 Connection engine for database.
 """
+
+class DBEngine:
+    def connect(self):
+        # Placeholder method to simulate database connection
+        print("Connecting to the database...")
